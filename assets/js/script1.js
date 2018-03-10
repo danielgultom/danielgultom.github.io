f3e43b5a0b1631c7883d717f2e15af89690e9388
danielhamg <danielhamg@gmail.com>
2018-03-10 15:24:43 -0800

    More dynamic text, star generation!
    Randomly generate stars. Currently is a mix of manually created stars and random ones, but still figuring out which to keep or if I would like to keep them both to maintain more even distribution. Also made them smaller.
    In addition, changed text size to be in terms of vw rather than px in the "description" section. Still deciding how to proceed with that part, which I plan to maybe have a typewriter effect.

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,3 +1,9 @@
+// window.scroll({
+//   top: 2500, 
+//   left: 0, 
+//   behavior: 'smooth' 
+// });
+
 var tCount = 0;
 var j = 0;
 var txt = "Welcome to my personal website! It's currently under heavy construction, but I hope you'll understand. I have plans to make it super cool! I began this website from scratch with very little web design experience, so thanks for being patient. I'm currently working on making it more mobile-friendly, so stay tuned for more cool stuff :D";
@@ -58,23 +64,28 @@ function blinkCursor() {
   setTimeout(blinkCursor, speedDone);
 
 }
+function makeStars(numStars) {
+  var starfield = document.getElementById("starfield");
+  for (i = 0; i < numStars; i++) {
+    var starLeft = Math.random() * 90 + 5;
+    // var starTop = Math.random() * starfield.style.height * .8 + 100;
+    var starTop = Math.random() * 3000 * .8;
 
+    console.log("bbb" + String(starfield.style.height)+"asdfasdfa");
+    // console.log(starTop);
+    // console.log(makeStarDiv(starLeft, starTop));
+    starfield.innerHTML += makeStarDiv(starLeft, starTop);
+  }
+}
 
-// function moveStars() {
-//   if (true) {
-//     document.getElementById("star1").style.top = parseInt(starLoc, 10) + window.scrollY+ "px";
-//     // console.log(document.getElementById("star1").style.top + window.scrollY);
-//     // console.log(window.scrollY);
-//     setTimeout(moveStars, 100);
-//   }
-  
-// }
+function makeStarDiv(horizInPercent, vertInPixels) {
+  return "<div class='stars' style='width:10px;height:10px; left: "+ horizInPercent + "%; top: " + vertInPixels + "px;'></div>";
+}
 
 
 
 $(document).ready(function() {
-
-
+  makeStars(50);
   var stars = document.getElementsByClassName("stars");
   var starLocs = [];
   getOrigLocs();
@@ -83,7 +94,7 @@ $(document).ready(function() {
 
   function setThings() {
     for (i = 0; i < stars.length; i++) { 
-      var size = Math.random() * 8 + 2;
+      var size = Math.random() * 4;
       stars[i].style.zIndex = "-1";
       stars[i].style.borderRadius = "50%";
       stars[i].style.width = size + "px";
@@ -93,141 +104,32 @@ $(document).ready(function() {
       // stars[i].style.background = "radial-gradient(white 1%, black 50%)";
 
     }
-  } setThings();
+  } 
+  
+  setThings();
+  // makeStars(100);
   function getSpeeds() {
     // var stars = document.getElementsByClassName("stars");
     // var speedsList = [];
     for (i = 0; i < stars.length; i++) { 
-      speedsList.push((Math.random() * 10) + 2);
+      speedsList.push((Math.random() * 1) + 2);
 
     }
-    console.log(speedsList);
+    // console.log(speedsList);
   }
-
+  
   function getOrigLocs() {
     for (i = 0; i < stars.length; i++) { 
       starLocs.push(stars[i].style.top);
     }
-    console.log(starLocs);
   }
 
   $(window).scroll(function(){
-    // window.scrollBy(0,1);
-    // scrolldelay = setTimeout(pageScroll,10);
-    // document.getElementById("star1").style.top = parseInt(starLoc, 10) + window.scrollY/15+ "px";
-    // console.log(parseInt(starLoc, 10) + window.scrollY/10);
-    // console.log(window.scrollY);
     for (i = 0; i < stars.length; i++) { 
       stars[i].style.top = parseInt(starLocs[i], 10) + window.scrollY/speedsList[i] + "px";
     }
   }); 
 
-// jQuery Function Number 1 (click)
-  $("#sidebar-button").click(function() {
-    // jQuery Function Number 2 (hasClass)
-    if ($('.sidebar-container').hasClass("sidebar-active")) {
-      // jQuery Function Number 3 (removeClass)
-      $('body').removeClass('no-scroll');
-      $('#sidebar-button').removeClass('button-active');
-      $('.sidebar-container').removeClass('sidebar-active');
-      $('.page-wrapper').removeClass('wrapper-active');
-    } else {
-      // jQuery Function Number 4 (addClass)
-      $('#sidebar-button').addClass('button-active');
-      $('.sidebar-container').addClass('sidebar-active');
-      $('.page-wrapper').addClass('wrapper-active');
-      setTimeout(function() {
-        $('body').addClass('no-scroll');
-      }, 300);
-    }
-
-  })
-
-
-
-  $(".page-wrapper").click(function() {
-    if ($('.sidebar-container').hasClass("sidebar-active")) {
-      $('body').removeClass('no-scroll');
-      $('#sidebar-button').removeClass('button-active');
-      $('.sidebar-container').removeClass('sidebar-active');
-      $('.page-wrapper').removeClass('wrapper-active');
-    } else {}
-  })
-  
-
-  $("#carousel-next").click(function() {
-    // jQuery Function Number 5 (css)
-    marleft = parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (marleft <= -960) {
-      return false
-    } else {
-      $('#carousel').css('margin-left', marleft - 960)
-    }
-  })
-
-  $("#carousel-prev").click(function() {
-      marleft = parseInt($('#carousel').css('margin-left').replace("px", ""));
-      if (marleft >= 0) {
-        return false
-      } else {
-        $('#carousel').css('margin-left', marleft + 960)
-      }
-
-  })
-
-// jQuery Function Number 6 (hover)
-  $("h2").hover(function () {
-    if ($(this).hasClass('active') != true) {
-      $(this).css("font-size", "28px")
-      $(this).addClass('active')
-
-    } else {
-      $(this).css("font-size", "24px")
-      $(this).removeClass('active')
-
-    }
-  })
-
-  $(".work").hover(function () {
-    console.log($(this).css("font-size"))
-    if ($(this).hasClass('active') != true) {
-      $(this).css("font-size", "28px")
-      $(this).addClass('active')
-      $("#workContent").css("display", "inline")
-    } else {
-      $(this).css("font-size", "24px")
-      $(this).removeClass('active')
-      $("#workContent").css("display", "none")
-    }
-
-    })
-
-  $(".edu").hover(function () {
-    console.log($(this).css("font-size"))
-    if ($(this).hasClass('active') != true) {
-      $(this).css("font-size", "28px")
-      $(this).addClass('active')
-      $("#eduContent").css("display", "inline")
-    } else {
-      $(this).css("font-size", "24px")
-      $(this).removeClass('active')
-      $("#eduContent").css("display", "none")
-    }
-  })
-
-  $(".projects").hover(function () {
-    console.log($(this).css("font-size"))
-    if ($(this).hasClass('active') != true) {
-      $(this).css("font-size", "28px")
-      $(this).addClass('active')
-      $("#projContent").css("display", "inline")
-    } else {
-      $(this).css("font-size", "24px")
-      $(this).removeClass('active')
-      $("#projContent").css("display", "none")
-
-    }
-  })
 });
 
 
