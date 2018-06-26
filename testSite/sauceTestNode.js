5be8a46eaaeaf16b2634d1d0f9d062c15eb4a0e5
dgultomcooper <dgultom@coopervision.com>
2018-06-26 12:57:32 -0700

    fixed part of the title crawl

--- testSite/sauceTestNode.js
+++ testSite/sauceTestNode.js
@@ -1,11 +1,5 @@
 
 
-
-// doesn't support the platform & version that this default code has 
-
-
-
-
 var webdriver = require('selenium-webdriver'),
     username = "dgultom",
     accessKey = "92b0227a-5413-438b-b0de-ef07f87f8dce",
@@ -14,6 +8,7 @@ var webdriver = require('selenium-webdriver'),
 driver = new webdriver.Builder().
   withCapabilities({
     'browserName': 'chrome',
+    // doesn't support the platform & version
     // 'platform': 'Windows XP',
     // 'version': '43.0',
     'username': username,
@@ -23,10 +18,62 @@ driver = new webdriver.Builder().
               "@ondemand.saucelabs.com:80/wd/hub").
   build();
  
-driver.get("http://saucelabs.com/test/guinea-pig");
+driver.get("https://danielhamg.github.io/testSite/homeTest");
+
+var document = driver.body;
+console.log(document);
+// runTests();
  
+
+
+
+
 driver.getTitle().then(function (title) {
-    console.log("title is: " + title);
+    // console.log("title is: " + title);
+    console.log("Title is: danielhamg?");
+    // runTests();
 });
  
 driver.quit();
+
+
+
+function findBrokenImages() {
+  var failedImages = [];
+  for (i = 0; i < document.getElementsByTagName("img").length; i++) {
+    var pic = document.getElementsByTagName("img")[i];
+    if (!pic.complete || pic.naturalHeight === 0) {
+      failedImages.push(pic.src);
+    } else {
+      ;
+    }
+  }
+
+  if (failedImages.length > 0) {
+    var failure = " ";
+    for (i = 0; i < failedImages.length; i++) {
+      failure += failedImages[i];
+      failure += "      ";
+    }
+    return("These images failed to load: " + failure);
+  } else {
+    return("No broken images found");
+  }
+}
+
+function findBrokenLinks() {
+  return("Have not yet implemented findBrokenLinks");
+}
+
+
+////////////////////////////////////////////////////////////
+/////////////////////////////////////////// running tests///
+////////////////////////////////////////////////////////////
+
+
+function runTests() {
+  console.log(findBrokenImages());
+  console.log(findBrokenLinks());
+}
+
+
